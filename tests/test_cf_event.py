8cd8e0e949ac277ed7cf03277f286400810cb7dc
bweigel <benjamin.weigel@europace.de>
2018-08-03 23:10:50 +0200

    [cf event tests] use pytest

--- tests/test_cf_event.py
+++ tests/test_cf_event.py
@@ -1,25 +1,39 @@
-from unittest import TestCase
+import pytest
 
 from aws_dataclasses.cf_event import CloudfrontEvent
 
 from .util import get_event_dict
 
 
-class TestProxyEvent(TestCase):
-    def setUp(self):
-        request_event_dict = get_event_dict("cf_request_event.json")
-        response_event_dict = get_event_dict("cf_response_event.json")
-        self.req_event = CloudfrontEvent.from_event(request_event_dict).first_record
-        self.resp_event = CloudfrontEvent.from_event(response_event_dict).first_record
+@pytest.fixture(scope="module")
+def cf_request_event_raw():
+    return get_event_dict("cf_request_event.json")
 
-    def test_request(self):
-        evt = self.req_event
-        self.assertEqual(evt.config.request_id, "MRVMF7KydIvxMWfJIglgwHQwZsbG2IhRJ07sn9AkKUFSHS9EXAMPLE==")
-        self.assertIsNone(evt.response)
-        self.assertEqual(evt.request.querystring, "size=large")
 
-    def test_response(self):
-        evt = self.resp_event
-        self.assertEqual(evt.config.request_id, "xGN7KWpVEmB9Dp7ctcVFQC4E-nrcOcEKS3QyAez--06dV7TEXAMPLE==")
-        self.assertEqual(evt.response.status, "200")
-        self.assertEqual(evt.request.client_ip, "2001:0db8:85a3:0:0:8a2e:0370:7334")
+@pytest.fixture(scope="module")
+def cf_response_event_raw():
+    return get_event_dict("cf_response_event.json")
+
+
+@pytest.fixture(scope="module")
+def cf_request_event(cf_request_event_raw):
+    return CloudfrontEvent.from_event(cf_request_event_raw)
+
+
+@pytest.fixture(scope="module")
+def cf_response_event(cf_response_event_raw):
+    return CloudfrontEvent.from_event(cf_response_event_raw)
+
+
+def test_request(cf_request_event):
+    evt = cf_request_event.first_record
+    assert evt.config.request_id == "MRVMF7KydIvxMWfJIglgwHQwZsbG2IhRJ07sn9AkKUFSHS9EXAMPLE=="
+    assert evt.response is None
+    assert evt.request.querystring == "size=large"
+
+
+def test_response(cf_response_event):
+    evt = cf_response_event.first_record
+    assert evt.config.request_id == "xGN7KWpVEmB9Dp7ctcVFQC4E-nrcOcEKS3QyAez--06dV7TEXAMPLE=="
+    assert evt.response.status == "200"
+    assert evt.request.client_ip == "2001:0db8:85a3:0:0:8a2e:0370:7334"
