bb08727a88a1c4f7956e0683dc3312416c5f81a0
bweigel <benjamin.weigel@europace.de>
2018-12-03 13:29:49 +0100

    updates requirements for compatibility with Python 3.7

--- setup.py
+++ setup.py
@@ -4,24 +4,18 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open(path.join(here, 'HISTORY.md'), encoding='utf-8') as f:
+with open(path.join(here, "HISTORY.md"), encoding="utf-8") as f:
     history = f.read()
 
 setup(
     name="python-aws-dataclasses",
     version="0.4.0",
     packages=find_packages(exclude=["tests"]),
-
-    install_requires=['dataclasses>=0.6',
-                      'arrow>=0.11.0'],
-
-    package_data={
-        '': ['*.txt', '*.rst', '*.md']
-    },
-
+    install_requires=["dataclasses>=0.6; python_version == '3.6'", "arrow>=0.11.0"],
+    package_data={"": ["*.txt", "*.rst", "*.md"]},
     author="Benjamin Weigel",
     author_email="benjamin.weigel@europace.de",
     description="Data classes for AWS stuff (lambda events etc.)",
@@ -34,7 +28,5 @@ setup(
         "Documentation": "http://github.com/hypoport/python-aws-dataclasses",
         "Source Code": "http://github.com/hypoport/python-aws-dataclasses",
     },
-
-    test_suite="tests"
-
+    test_suite="tests",
 )
