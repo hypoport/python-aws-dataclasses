9885f9c8b635c42f0bd58514c2131d0417b5ef49
bweigel <benjamin.weigel@europace.de>
2019-01-29 17:29:06 +0100

    update imports

--- aws_dataclasses/__init__.py
+++ aws_dataclasses/__init__.py
@@ -2,10 +2,20 @@
 python-aws-dataclasses
 """
 
-__version__="0.4.5"
+__version__ = "0.4.5"
+
+__all__ = [
+    "SnsEvent",
+    "AlexaSkillEvent",
+    "S3Event",
+    "CloudFrontEvent",
+    "ApiGwProxyEvent",
+    "KinesisDataStreamEvent",
+]
 
 from aws_dataclasses.sns_event import SnsEvent
 from aws_dataclasses.alexa_skill_event import AlexaSkillEvent
 from aws_dataclasses.s3_event import S3Event
 from aws_dataclasses.cf_event import CloudFrontEvent
 from aws_dataclasses.http_proxy_event import ApiGwProxyEvent
+from aws_dataclasses.kinesis_datastream_event import KinesisDataStreamEvent
